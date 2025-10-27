4c9adb8a1a4f037b8f101c5fc536f9816c0cfc00
nadiachaja <nadiagaja13@gmail.com>
2025-10-27 15:56:05 +0100

    #1 link weggehaald nav

--- my-app/components/Header.jsx
+++ my-app/components/Header.jsx
@@ -6,7 +6,6 @@ export default function Header() {
         <ul>
             <li><Link href={`/`}>Home</Link></li>
             <li><Link href={`/blog`}>Blog</Link></li>
-            <li><Link href={`/blog/post`}>Post</Link></li>
         </ul>
         </nav>
     );
