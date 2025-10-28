7c862c77fb14ce3c86f73f2349b6467da91455d2
nadiachaja <nadiagaja13@gmail.com>
2025-10-28 17:52:33 +0100

    #1  Content Management eXperience

--- my-app/app/blog/post/page.jsx
+++ my-app/app/blog/post/page.jsx
@@ -5,6 +5,12 @@ export default function Page() {
       <h1>Hello, Post</h1>
 
       <a href="https://www.hva.nl/">hva</a>
+
+      <h2>Boek Blog</h2>
+
+      <p>Welkom bij deze boek blog!</p>
+
+
       </>
     )
   
