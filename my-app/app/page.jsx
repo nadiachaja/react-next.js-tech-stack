6ad53cf98bdd47fb7273a9dbf28ab43e353a57ff
nadiachaja <nadiagaja13@gmail.com>
2025-10-28 12:25:58 +0100

    #1 img veranderen

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -3,8 +3,17 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <>
-    <Image src="/next.svg" alt="NextImg" width={100} height={100} />
+    <Image src="/IMG_2743.jpg" alt="NextImg" width={300} height={480} />
     <h1>Hello, Next.js!</h1>
+
+    <p>Ik ga testen hoe next.js en react werken om zo te kijken welke framework het beste is en welke ik het fijnst vind werken.</p>
+
+    <form action="">
+      <input type="search" />
+      <button>search</button>
+
+      <input type="radio" />
+    </form>
     </>
   )
 
